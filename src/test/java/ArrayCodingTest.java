82edb93bdc6c0b09b334c9054b204abd80a3d7dd
meengi <meengi@korpay.com>
2023-10-16 23:25:02 +0900

    238. Product of Array Except Self

--- src/test/java/ArrayCodingTest.java
+++ src/test/java/ArrayCodingTest.java
@@ -153,5 +153,30 @@ public class ArrayCodingTest {
         return sum;
     }
 
+    @Test
+    @DisplayName("238. Product of Array Except Self")
+    void productExceptSelf() {
+        int[] nums = new int[]{1,2,3,4};
+        int[] result = productExceptSelf(nums);
+        assertArrayEquals(result, new int[]{24,12,8,6});
+    }
+
+    private int[] productExceptSelf(int[] nums) {
+        int[] result = new int[nums.length];
+
+        int p = 1;
+        for(int i=0; i<nums.length; i++) {
+            result[i] = p;
+            p *= nums[i];
+        }
+        p = 1;
+        for (int i=nums.length-1; i>=0; i--) {
+            result[i] *= p;
+            p *= nums[i];
+        }
+
+        return result;
+    }
+
 
 }
