106897ea01d544e9d5baa395eb9e7456b79ddb4a
meengi <meengi@korpay.com>
2023-10-30 22:14:09 +0900

    206. Reverse Linked List

--- src/test/java/ArrayCodingTest.java
+++ src/test/java/ArrayCodingTest.java
@@ -261,4 +261,23 @@ public class ArrayCodingTest {
             return list2;
         }
     }
+
+    @Test
+    @DisplayName("206. Reverse Linked List")
+    void reverseListTest() {
+        reverseList(new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5))))));
+
+    }
+
+    ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while(curr != null) {
+            ListNode nextTemp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextTemp;
+        }
+        return prev;
+    }
 }
